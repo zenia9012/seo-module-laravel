c7166f58a4c9c6d0702a1dececc7a3bca3f3770d
Yevhenii <yevhenii.teamscale@gmail.com>
2020-03-16 18:29:51 +0200

    show info in table

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -3,11 +3,15 @@
 namespace Yevhenii\Seo\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Yevhenii\Seo\Models\Seo;
 
 class AdminController extends Controller {
 
     public function index(Request $request)
     {
-        return view('seo::admin.main');
+        $columns = config('seo.show_columns');
+        $pages = Seo::all($columns);
+
+        return view('seo::admin.main', compact(['pages', 'columns']));
     }
 }
--- src/config/seo.php
+++ src/config/seo.php
@@ -8,4 +8,15 @@ return [
     'og_title' => env('APP_NAME'),
     'og_description' => env('APP_NAME'),
     'og_type' => env('APP_NAME'),
+
+    'show_columns' => [
+        'slug_page',
+        'title',
+        'description',
+        'keywords',
+        'og_title',
+        'og_image',
+        'og_description',
+        'og_type',
+    ],
 ];
